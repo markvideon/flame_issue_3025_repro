cff70e8b3620050687193933888ff54c225b0ec0
markvideon <mark.videon@gmail.com>
2024-03-15 20:25:51 +1030

    Added reproduction of issue

---
+++ android/app/src/main/kotlin/com/example/flame_issue_3025_repro/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flame_issue_3025_repro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'dart:async';
+
+import 'package:flame/components.dart' hide Timer;
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame_riverpod/flame_riverpod.dart';
+import 'package:flutter/material.dart' hide Route;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final countingStreamProvider = StreamProvider<int>((ref) {
+  return Stream.periodic(const Duration(seconds: 1), (inc) => inc);
+});
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+final gameInstance = RefExampleGame();
+final GlobalKey<RiverpodAwareGameWidgetState> gameWidgetKey =
+GlobalKey<RiverpodAwareGameWidgetState>();
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Expanded(child: FlutterCountingComponent()),
+          Expanded(
+            child: RiverpodAwareGameWidget(
+              key: gameWidgetKey,
+              game: gameInstance,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FlutterCountingComponent extends ConsumerWidget {
+  const FlutterCountingComponent({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textStyle = Theme.of(context)
+        .textTheme
+        .headlineSmall
+        ?.copyWith(color: Colors.white);
+
+    final stream = ref.watch(countingStreamProvider);
+    return Material(
+      color: Colors.transparent,
+      child: Column(
+        children: [
+          Text('Flutter', style: textStyle),
+          stream.when(
+            data: (value) => Text('$value', style: textStyle),
+            error: (error, stackTrace) => Text('$error', style: textStyle),
+            loading: () => Text('Loading...', style: textStyle),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RefExampleGame extends FlameGame with RiverpodGameMixin {
+  late RouterComponent _router;
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    add(ButtonComponent(
+      onPressed: () {
+        print('push home');
+        _router.pushReplacementNamed('home');
+      },
+      button: RectangleComponent(
+        size: Vector2(100,100),
+        paint: Paint()..color = Colors.red,
+        children: [
+          TextComponent(text: 'home')
+        ]
+      ),
+    ));
+    add(ButtonComponent(
+      position: Vector2(100,0),
+      onPressed: () {
+        print('push play');
+        _router.pushReplacementNamed('play');
+      },
+      button: RectangleComponent(
+        size: Vector2(100,100),
+        paint: Paint()..color = Colors.red,
+        children: [
+          TextComponent(text: 'play')
+        ]
+      ),
+    ));
+
+    world.add(
+      _router = RouterComponent(
+        routes: {
+          'home': Route(HomePage.new, maintainState: true),
+          'play': Route(PlayPage.new, maintainState: true),
+        },
+        initialRoute: 'home',
+      ),
+    );
+  }
+}
+
+class HomePage extends Component
+    with HasGameReference<RefExampleGame>, RiverpodComponentMixin {
+}
+
+class PlayPage extends Component
+    with HasGameReference<RefExampleGame>, RiverpodComponentMixin {
+}
+
+class RiverpodAwareTextComponent extends PositionComponent
+    with RiverpodComponentMixin {
+  late TextComponent textComponent;
+  int currentValue = 0;
+
+  /// [onMount] should be used over [onLoad] to initialize subscriptions,
+  /// which is only called if the [Component] was mounted.
+  /// Cancellation is handled for the user automatically inside [onRemove].
+  ///
+  /// [RiverpodComponentMixin.addToGameWidgetBuild] **must** be invoked in
+  /// your Component **before** [RiverpodComponentMixin.onMount] in order to
+  /// have the provided function invoked on
+  /// [RiverpodAwareGameWidgetState.build].
+  ///
+  /// From `flame_riverpod` 5.0.0, [WidgetRef.watch], is also accessible from
+  /// components.
+  @override
+  void onMount() {
+    addToGameWidgetBuild(() {
+      ref.listen(countingStreamProvider, (p0, p1) {
+        if (p1.hasValue) {
+          currentValue = p1.value!;
+          textComponent.text = '$currentValue';
+        }
+      });
+    });
+    super.onMount();
+    add(textComponent = TextComponent(position: position + Vector2(0, 27)));
+  }
+}
